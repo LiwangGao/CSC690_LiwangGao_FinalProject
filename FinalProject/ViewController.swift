068f16f6d5e322621bfe5ad292291d55f9254136
liwanggao <lgao2@mail.sfsu.edu>
2018-12-13 18:46:27 -0800

    11

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -5,6 +5,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
